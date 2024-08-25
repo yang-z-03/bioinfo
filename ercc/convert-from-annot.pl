9da0074b4e7cd5d97c5d9a5c13d57d6ecb5efcfe
yang-z <xornent@outlook.com>
2024-08-25 11:23:58 +0800

    licensing

--- ercc/convert-from-annot.pl
+++ ercc/convert-from-annot.pl
@@ -1,34 +1,41 @@
 use strict;
 use warnings;
-# Converts the Annotation file from https://www.thermofisher.com/order/catalog/product/4456740 into fasta and gtf files that can be added to the end of an existing genome fasta/gtf
 
-my @FASTAlines = ();
-my @GTFlines = ();
-open (my $ifh, "ERCC_Controls_Annotation.txt") or die $!;
-<$ifh>; #header
+# Converts the annotation file into fasta and gtf files that can be added 
+# to the end of an existing genome fasta/gtf. You may obtain the annotation file from:
+# https://www.thermofisher.com/order/catalog/product/4456740
+
+my @fasta_lines = ();
+my @gtf_lines = ();
+open (my $ifh, "annotation.txt") or die $!;
+<$ifh>;  # skip the header line in tha annotation
+
 while (<$ifh>) {
-	# Do all the important stuff
-	chomp;
+	chomp;  # do all the important stuff
 	my @record = split(/\t/);
 	my $sequence = $record[4];
-	$sequence =~ s/\s+//g; # get rid of any preceeding/tailing white space
-	$sequence = $sequence."NNNN"; # add some buffer to the end of the sequence
+	$sequence =~ s/\s+//g;  # get rid of any preceeding/tailing white space
+	$sequence = $sequence."NNNN";  # add some buffer to the end of the sequence
 	my $name = $record[0];
 	my $genbank = $record[1];
-	push(@FASTAlines, ">$name\n$sequence\n");
-# is GTF 1 indexed or 0 indexed? -> it is 1 indexed
-# + or - strand?
-	push(@GTFlines, "$name\tERCC\tgene\t1\t".(length($sequence)-2)."\t.\t+\t.\tgene_id \"$name-$genbank\"; transcript_id \"$name-$genbank\"; exon_number \"1\"; gene_name \"ERCC $name-$genbank\"\n");
-	push(@GTFlines, "$name\tERCC\texon\t1\t".(length($sequence)-2)."\t.\t+\t.\tgene_id \"$name-$genbank\"; transcript_id \"$name-$genbank\"; exon_number \"1\"; gene_name \"ERCC $name-$genbank\"\n");
+	push(@fasta_lines, ">$name\n$sequence\n");
+	
+    # the sequences are all '+' stranded. and the gtf files are 1 indexed.
+	push(@gtf_lines, "$name\tERCC\tgene\t1\t" . (length($sequence)-2) . "\t.\t+\t." .
+	                 "\tgene_id \"$name-$genbank\"; transcript_id \"$name-$genbank\"; " .
+	                 "exon_number \"1\"; gene_name \"ERCC $name-$genbank\"\n");
+	push(@gtf_lines, "$name\tERCC\texon\t1\t" . (length($sequence)-2) . "\t.\t+\t." .
+	                 "\tgene_id \"$name-$genbank\"; transcript_id \"$name-$genbank\"; " .
+	                 "exon_number \"1\"; gene_name \"ERCC $name-$genbank\"\n");
 } close($ifh);
 
-# Write output
-open(my $ofh, ">", "ERCC_Controls.fa") or die $!;
-foreach my $line (@FASTAlines) {
+# write output fasta and gtf files.
+open(my $ofh, ">", "ercc.fasta") or die $!;
+foreach my $line (@fasta_lines) {
 	print $ofh $line;
 } close ($ofh);
 
-open($ofh, ">", "ERCC_Controls.gtf") or die $!;
-foreach my $line (@GTFlines) {
+open($ofh, ">", "ercc.gtf") or die $!;
+foreach my $line (@gtf_lines) {
 	print $ofh $line;
 } close ($ofh);
